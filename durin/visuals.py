e8140f5a1ba7c2d83c5a3015550406424673dab4
Jegp <jeped@kth.se>
2022-05-04 21:17:18 +0200

    Primitive dashboard (part II)

--- durin/visuals.py
+++ durin/visuals.py
@@ -2,7 +2,7 @@
 import multiprocessing 
 import numpy as np
 
-TOF_WIDTH = 8*8
+TOF_WIDTH = 8 #8*8
 TOF_HEIGHT = 8
 
 DVS_HEIGHT = 480
@@ -11,16 +11,37 @@ DVS_WIDTH = 640
 
 import cv2
 
-def vis_process(dvs_np):
+    
 
-    cv2.namedWindow('win1')
+def vis_process(dvs_np, tof_np):
 
+    cv2.namedWindow('Dashboard')
+
+    ext = 120
+    img_mat = np.zeros((DVS_HEIGHT, DVS_WIDTH+ext*4,3))
     dvs_px_mat = np.zeros((DVS_HEIGHT,DVS_WIDTH,3))
+    tof_px_mat = np.zeros((TOF_HEIGHT,TOF_WIDTH,3))
 
+
+    font = cv2.FONT_HERSHEY_TRIPLEX
+    org = (830, 60)
+    fontScale = 1
+    color = (255, 255, 255)
+    thickness = 2
+    md = 2000 # 
 
     while True:
-        dvs_px_mat[:,:,2] = dvs_np*255
-        cv2.imshow('win1', dvs_px_mat)
+
+        helper = (tof_np)/md
+        tof_px_mat[:,:,1] = np.where(helper>md, md, helper)
+        img_mat[ext-1:3*ext+1,ext+DVS_WIDTH-1:DVS_WIDTH+ext*3+1,:] = np.ones((2*ext+2,2*ext+2,3))*255
+        img_mat[ext:3*ext,ext+DVS_WIDTH:DVS_WIDTH+ext*3,:] = cv2.resize(tof_px_mat,(2*ext, 2*ext), interpolation = cv2.INTER_NEAREST)
+        img_mat[:,0:DVS_WIDTH,2] = np.transpose(dvs_np*255)
+
+        img_mat = cv2.putText(img_mat, "Durin", org, font, fontScale, color, thickness, cv2.LINE_AA)
+        
+
+        cv2.imshow('Dashboard', img_mat)
         cv2.waitKey(1) 
 
 
@@ -30,9 +51,14 @@ def launch_visual():
 
     manager = multiprocessing.Manager()
 
-    dvs_mp = multiprocessing.Array('I', int(np.prod((DVS_HEIGHT, DVS_WIDTH))), lock=multiprocessing.Lock())
-    dvs_np = np.frombuffer(dvs_mp.get_obj(), dtype='I').reshape((DVS_HEIGHT, DVS_WIDTH))
+    dvs_mp = multiprocessing.Array('I', int(np.prod((DVS_WIDTH, DVS_HEIGHT))), lock=multiprocessing.Lock())
+    dvs_np = np.frombuffer(dvs_mp.get_obj(), dtype='I').reshape((DVS_WIDTH, DVS_HEIGHT))
 
-    p_visual = multiprocessing.Process(target=vis_process, args=(dvs_np,))
+    tof_mp = multiprocessing.Array('I', int(np.prod((TOF_HEIGHT, TOF_WIDTH))), lock=multiprocessing.Lock())
+    tof_np = np.frombuffer(tof_mp.get_obj(), dtype='I').reshape((TOF_HEIGHT, TOF_WIDTH))
 
-    return p_visual, dvs_np
+
+
+    p_visual = multiprocessing.Process(target=vis_process, args=(dvs_np, tof_np,))
+
+    return p_visual, dvs_np, tof_np
