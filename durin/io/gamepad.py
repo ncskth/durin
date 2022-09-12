027a292abdb151ed4f518f7bbdc125d27525a808
Jens E. Pedersen <jensegholm@protonmail.com>
2022-09-12 13:03:57 +0200

    Corrected predictions and caught gamepad error

--- durin/io/gamepad.py
+++ durin/io/gamepad.py
@@ -5,46 +5,45 @@ from inputs import get_gamepad, UnpluggedError
 from durin.io.runnable import RunnableProducer
 
 
+JOYSTICK_MIN = 21
+JOYSTICK_MAX = 232
+JOYSTICK_ABS = JOYSTICK_MAX - JOYSTICK_MIN
+JOYSTICK_MID = JOYSTICK_ABS / 2
+
+
 class Gamepad(RunnableProducer):
     def __init__(self, timeout: float = 0.05):
         context = multiprocessing.get_context("spawn")
         self.queue = context.Queue(maxsize=1)
         self.timeout = timeout
-        self.values = multiprocessing.Array("i", 3)
+        self.values = multiprocessing.Array("f", 3)
+        self.tau = 0.99
 
         super().__init__(self.queue, self.values)
 
     @staticmethod
     def norm(x):
-        MIN = 21
-        MAX = 232
-        ABS = MAX - MIN
-        MID = ABS / 2
         if x == 0:
-            return x
+            return int(x)
         else:
-            return ((x - MID - MIN) / ABS) * 1000
-
-    def start(se1lf):
-        try:
-            get_gamepad()
-            super().start()
-        except UnpluggedError:
-            pass
+            return int(((x - JOYSTICK_MID - JOYSTICK_MIN) / JOYSTICK_ABS) * 1000)
 
     def produce(self, values):
         array = values.get_obj()
 
         # Register gamepad events
-        events = get_gamepad()
-        for event in events:
-            stick = event.code
-            state = event.state
-            if state > 0:
-                if "ABS_X" in stick:
-                    array[0] = state
-                elif "ABS_Y" in stick:
-                    array[1] = state
-                elif "ABS_RX" in stick:
-                    array[2] = state
+        try:
+            events = get_gamepad()
+            for event in events:
+                stick = event.code
+                state = event.state
+                if state > 0:
+                    if "ABS_X" in stick:
+                        array[0] = state
+                    elif "ABS_Y" in stick:
+                        array[1] = state
+                    elif "ABS_RX" in stick:
+                        array[2] = state
+        except UnpluggedError:
+            pass
         return Gamepad.norm(array[0]), Gamepad.norm(array[1]), Gamepad.norm(array[2])
