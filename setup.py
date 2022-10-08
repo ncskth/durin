4a72ca909df096fbd37b46b4c228f4316bd19e4e
ncsdurin <ncsdurin@kth.se>
2022-10-08 11:54:06 +0000

    incremented version number -- with love from a robot

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as fp:
 
 setup(
     name="durin",
-    version="0.0.43",
+    version="0.0.44",
     install_requires=requirements,
     packages=["durin"],
     license="LGPLv3",
