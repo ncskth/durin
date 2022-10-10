b4d46be13f628ccb95f142b200fa34a265dc0c52
[Bot]Durin <noreply.ncsdurin@kth.se>
2022-10-10 07:35:47 +0000

    This is the action of github actions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as fp:
 
 setup(
     name="durin",
-    version="0.0.48",
+    version="0.0.49",
     install_requires=requirements,
     packages=["durin"],
     license="LGPLv3",
