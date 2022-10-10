36bc72125f64564ca72488777db9b05619ae1419
Johannes lusk <lusk@kth.se>
2022-10-10 09:32:19 +0200

    Hope this works atleast

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("requirements.txt", "r") as fp:
 
 setup(
     name="durin",
-    version="0.0.47",
+    version="0.0.48",
     install_requires=requirements,
     packages=["durin"],
     license="LGPLv3",
