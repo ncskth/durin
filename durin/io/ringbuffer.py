9f34ea071152443f17f58778d60e4f34c14f7914
Jens E. Pedersen <jensegholm@protonmail.com>
2023-03-09 20:13:52 +0100

    Added period information for tof,imu,uwb,status in observation

--- durin/io/ringbuffer.py
+++ durin/io/ringbuffer.py
@@ -2,13 +2,20 @@ import numpy as np
 
 
 class RingBuffer(object):
-    def __init__(self, array: np.ndarray):
+    def __init__(self, array: np.ndarray, counter: int = 0):
         self.size = len(array)
         self.buffer = array
-        self.counter = 0
+        self.counter = counter
 
     def append(self, data):
         self.buffer[self.counter] = data
         self.counter += 1
         self.counter = self.counter % self.size
         return self.buffer
+
+    def get_oldest(self):
+        return self.buffer[self.counter]
+
+    def get_newest(self):
+        return self.buffer[(self.counter - 1 ) % self.size]
+
--- durin/sensor.py
+++ durin/sensor.py
@@ -16,11 +16,14 @@ T = TypeVar("T")
 
 class Observation(NamedTuple):
     tof: np.ndarray = np.zeros((8, 8, 8))
+    tof_frequency: float = 0
     charge: float = 0
     voltage: float = 0
     imu: np.ndarray = np.zeros((3, 3))
+    imu_frequency: float = 0
     uwb: float = 0
-    frequency: float = 0
+    uwb_frequency: float = 0
+    status_frequency: float = 0
 
     def __repr__(self) -> str:
         tof_str = " ".join([f"{x.mean():.0f}±{x.std():.0f}" for x in self.tof])
@@ -34,41 +37,65 @@ class Sensor(ABC, Generic[T]):
 
 
 class DurinSensor(RunnableConsumer, Sensor[Observation]):
+
+    _BUFFER_LENGTH = 50
+
     def __init__(self, link: UDPLink):
         self.link = link
         context = multiprocessing.get_context("spawn")
+
         self.tof = context.Array("f", 8 * 8 * 8)
+        self.tof_ringbuffer = context.Array("d", self._BUFFER_LENGTH)
+        self.tof_ringbuffer_idx = context.Value("i", 0)
+
         self.charge = context.Value("f", 0)
         self.voltage = context.Value("f", 0)
+        self.status_ringbuffer = context.Array("d", self._BUFFER_LENGTH)
+        self.status_ringbuffer_idx = context.Value("i", 0)
+
         self.imu = context.Array("d", 3 * 3)
+        self.imu_ringbuffer = context.Array("d", self._BUFFER_LENGTH)
+        self.imu_ringbuffer_idx = context.Value("i", 0)
+
         self.uwb = context.Value("d", 0)
-        self.ringbuffer = context.Array("d", 50)
-        self.ringbuffer_idx = context.Value("i", 0)
-        self.timestamp_update = context.Value("d", time.time())
+        self.uwb_ringbuffer = context.Array("d", self._BUFFER_LENGTH)
+        self.uwb_ringbuffer_idx = context.Value("i", 0)
 
         super().__init__(
             self.link.buffer,
             self.tof,
+            self.tof_ringbuffer,
+            self.tof_ringbuffer_idx,
             self.charge,
             self.voltage,
+            self.status_ringbuffer,
+            self.status_ringbuffer_idx,
             self.imu,
+            self.imu_ringbuffer,
+            self.imu_ringbuffer_idx,
             self.uwb,
-            self.ringbuffer,
-            self.ringbuffer_idx,
-            self.timestamp_update,
+            self.uwb_ringbuffer,
+            self.uwb_ringbuffer_idx
         )
+
+    def _read_buffer_frequency(self, buffer, index):
+        buffer = RingBuffer(buffer.get_obj(), index.value)
+        difference = buffer.get_newest() - buffer.get_oldest()
+        return self._BUFFER_LENGTH / difference
 
     def read(self) -> Observation:
         tof = np.frombuffer(self.tof.get_obj(), dtype=np.float32).reshape((8, 8, 8))
         imu = np.frombuffer(self.imu.get_obj()).reshape((3, 3))
-        frequency = 1 / (np.frombuffer(self.ringbuffer.get_obj()).mean() + 1e-7)
         return Observation(
-            tof,
+            tof=tof,
+            tof_frequency=self._read_buffer_frequency(self.tof_ringbuffer, self.tof_ringbuffer_idx),
             charge=self.charge.value,
             voltage=self.voltage.value,
+            status_frequency=self._read_buffer_frequency(self.status_ringbuffer, self.status_ringbuffer_idx),
             imu=imu,
+            imu_frequency=self._read_buffer_frequency(self.imu_ringbuffer, self.imu_ringbuffer_idx),
             uwb=self.uwb.value,
-            frequency=frequency,
+            uwb_frequency=self._read_buffer_frequency(self.uwb_ringbuffer, self.uwb_ringbuffer_idx),
         )
 
     def start(self):
@@ -83,13 +110,18 @@ class DurinSensor(RunnableConsumer, Sensor[Observation]):
         self,
         item,
         tof,
+        tof_ringbuffer,
+        tof_ringbuffer_idx,
         charge,
         voltage,
+        status_ringbuffer,
+        status_ringbuffer_idx,
         imu,
+        imu_ringbuffer,
+        imu_ringbuffer_idx,
         uwb,
-        ringbuffer,
-        ringbuffer_idx,
-        timestamp_update,
+        uwb_ringbuffer,
+        uwb_ringbuffer_idx
     ):
         which = item.which()
         try:
@@ -97,18 +129,20 @@ class DurinSensor(RunnableConsumer, Sensor[Observation]):
                 for obs in item.tofObservations.observations:
                     data = np.array(obs.ranges)
                     tof.get_obj()[obs.id * 64: (obs.id+1) * 64] = data
+                self._update_frequency(tof_ringbuffer, tof_ringbuffer_idx)
             elif which == "systemStatus":
                 charge.value = item.systemStatus.batteryPercent
                 voltage.value = item.systemStatus.batteryMv
-            # TODO: Add more sensors
+                self._update_frequency(status_ringbuffer, status_ringbuffer_idx)
 
-            # Update Hz
-            time_now = time.time()
-            buffer = RingBuffer(np.frombuffer(ringbuffer.get_obj()))
-            buffer.counter = ringbuffer_idx.value
-            buffer.append(time_now - timestamp_update.value)
-            ringbuffer.get_obj()[:] = buffer.buffer
-            ringbuffer_idx.value = buffer.counter
-            timestamp_update.value = time_now
+            # TODO: Add more sensors
         except Exception as e:
             logging.warning("Error when receiving sensor data " + str(e))
+
+    def _update_frequency(self, buffer_name, buffer_idx):
+        buffer_obj = buffer_name.get_obj()
+        buffer = RingBuffer(np.frombuffer(buffer_obj), buffer_idx.value)
+        buffer.append(time.time())
+        buffer_obj[:] = buffer.buffer
+        buffer_idx.value = buffer.counter
+        
