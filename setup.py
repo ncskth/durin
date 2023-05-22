d94828f52afb60afce131059dfe971c4cd5f9649
[Bot]Durin <noreply.ncsdurin@kth.se>
2023-05-22 11:51:52 +0000

    This is the action of github actions

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fp:
 
 setup(
     name="durin",
-    version="0.0.80",
+    version="0.0.81",
     install_requires=requirements,
     packages=find_packages(),
     license="LGPLv3",
